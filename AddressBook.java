7c822660b57ddf1b1578f5338b8690d96749d3f7
Ashish Sahu <aajisahu@gmail.com>
2021-09-26 17:01:06 +0530

    Adding: taking input from user

--- AddressBook.java
+++ AddressBook.java
@@ -1,21 +1,21 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import Day8_Programs.PresentAbsent;
 public class AddressBook {
+	private static final Logger Log= LogManager.getLogger(PresentAbsent.class);
     public static void main(String[] args) {
-        System.out.println("Welcome to the Address Book System code !!!");
-        contactDetails newContact = new contactDetails();
-        newContact.setFirstName("Ashish \n");
-        newContact.setLastName("Sahu \n");
-        newContact.setAddressCity("BBSR \n");
-        newContact.setState("Odisha \n");
-        newContact.setZip(740681) ;
-        newContact.setEmail("@gmail.com \n");
-        newContact.setPhoneNumber( 9078666455l);
-        System.out.println("The Contact is : \n" +newContact);
+        Log.info("Welcome to the Address Book System code !!!");
+        newAddressBook person = new newAddressBook();
+        person.addContact();
     }
 }
 
 class contactDetails {
+	private static final Logger Log= LogManager.getLogger(PresentAbsent.class);
     private String firstName;
     private String lastName;
     private String addressCity;
@@ -94,5 +94,38 @@ class contactDetails {
                         " PhoneNumber : " + phoneNumber +'\'' +
                         " Email : " + email;
 
+    }
+}
+ class newAddressBook {
+
+    Scanner sc = new Scanner(System.in);
+    private static final Logger Log= LogManager.getLogger(PresentAbsent.class);
+
+    public void addContact() {
+        contactDetails person = new contactDetails();
+        Log.info("Enter First Name: ");
+        String firstName = sc.nextLine();
+        Log.info("Enter last Name: ");
+        String lastName = sc.nextLine();
+        Log.info("Enter your addressCity: ");
+        String addressCity = sc.nextLine();
+        Log.info("Enter your state: ");
+        String state = sc.nextLine();
+        Log.info("Enter zip code : ");
+        Long zip = sc.nextLong();
+        sc.nextLine();
+        Log.info("Enter phone number: ");
+        Long phoneNumber = sc.nextLong();
+        sc.nextLine();
+        Log.info("Enter your EMail ID: ");
+        String email = sc.nextLine();
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+        person.setAddressCity(addressCity);
+        person.setState(state);
+        person.setZip(zip.intValue());
+        person.setPhoneNumber(phoneNumber.intValue());
+        person.setEmail(email);
+        Log.info("The Contact Details of "+firstName+ "\n"+ person);
     }
 }
